0570d7e31bf0d8121ce0b6005385d0e98d189d05
reinaldoasf <reinaldoasf@gmail.com>
2020-12-27 23:44:57 -0300

    commented javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//var nome = "Reinaldo Agostinho"
+//
+//var idade= 26;
+//var idade2 = 10;
+//var frase = "Aqui é Japão";
+////alert(idade2 + idade + " anos");
+//
+//console.log(nome);
+//console.log(idade2 * idade);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toLowerCase());
+//
+//console.log(frase.toUpperCase());
+//
