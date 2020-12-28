c61a2a273c9a3baeb8f9fbe3e3ed0eedbb2372a1
reinaldoasf <reinaldoasf@gmail.com>
2020-12-28 00:19:54 -0300

    added date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
+var d = new Date();
+
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getDate());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+
+
+//var count = 0;
+//while (count<=5)
+//{
+//    console.log(count);
+//    count++;
+//
+//
+//}
+//
+//
+//for( count=0;count<=5;count++ ){
+//    alert(count);
+//}
+
+
+//var idade = 18;
+/*
+var idade = prompt("Qual sua idade?");
+if(idade>=18)
+    alert("maior de idade");
+else
+    alert("menor de idade");
+*/
+/*
 var frutas = [{nome: "maçã", cor: "vermelha" },{nome: "uva", cor: "roxa" }];
 console.log(frutas[1].nome);
 
 alert(frutas[0].cor);
 
-/*var fruta = {nome: "maçã", cor: "vermelha" };
+var fruta = {nome: "maçã", cor: "vermelha" };
 console.log(fruta.nome);
 
 alert(fruta.cor);*/
